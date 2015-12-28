ccc6dcc5c5afe537742fc17483f7e9d9f09c0cbf
liyang31tg <liyang@macBookProdeMacBook-Pro.local>
2015-12-28 23:08:55 +0800

    i

--- app.js
+++ app.js
@@ -2,14 +2,22 @@
  * Created by liyang on 15/12/26.
  */
 var koa = require('koa.io');
+var views = require('koa-views');
+var route = require('koa-route');
 
 var app = koa();
 
 
-// middleware for koa
-app.use(function*() {
-});
-
+app.use(function *(next){
+    yield  next;
+    if (404 != this.status) return;
+    this.status = 404;
+    yield  this.render('./views/error')
+})
+app.use(views());
+app.use(route.get('/', function *(){
+    yield  this.render('./index');
+}));
 
 // middleware for socket.io's connect and disconnect
 app.io.use(function* (next) {
